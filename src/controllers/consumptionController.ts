a362931f315c8720ad0571c0ddb00a3d921e7e63
Mete Oğuzhan Bayrampınar <metebayrampinar@gameguru.net>
2024-01-25 15:25:14 +0300

    Fix sort for consumption list.

--- src/controllers/consumptionController.ts
+++ src/controllers/consumptionController.ts
@@ -7,7 +7,7 @@ import { ConsumptionModel } from "../models/ConsumptionModel"
 export default class ConsumptionController implements ConsumptionControllerInterface {
     static async index(req: Request, res: Response): Promise<void> {
         try {
-            const consumptionList = await ConsumptionModel.find({ userId: req?.user?._id }).exec()
+            const consumptionList = await ConsumptionModel.find({ userId: req?.user?._id }).sort({ date: -1 }).exec()
             if (!consumptionList.length) {
                 return sendErrorResponse(res, 1011)
             }
